808e51055db13f7fce1865ba82ba9d2490b762af
shaojiewang <wsjmessi@163.com>
2019-11-02 19:23:42 +0800

    1.got a baseline result

--- titanic/titanic_problem.py
+++ titanic/titanic_problem.py
@@ -4,6 +4,8 @@ from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestRegressor
+import sklearn.preprocessing as preprocessing
+from sklearn import linear_model
 
 data_train = pd.read_csv("./input/Train.csv")
 data_train.info()
@@ -150,3 +152,67 @@ def set_cabin_type(df):
 data_train, rfr = set_missing_ages(data_train)
 data_train = set_cabin_type(data_train)
 print(data_train)
+
+#把yes no等文字属性转换为one hot属性
+dummies_cabin = pd.get_dummies(data_train['Cabin'], prefix='Cabin')
+dummies_sex = pd.get_dummies(data_train['Sex'], prefix='Sex')
+dummies_pclass = pd.get_dummies(data_train['Pclass'], prefix='Pclsass')
+dummies_embarked = pd.get_dummies(data_train['Embarked'], prefix='Embarked')
+
+df = pd.concat([data_train, dummies_cabin, dummies_embarked, dummies_pclass, dummies_sex], axis=1)
+df.drop(['Name', 'Cabin', 'Sex', 'Pclass', 'Ticket', 'Embarked'], axis=1, inplace=True)
+print(df)
+
+scaler = preprocessing.StandardScaler()
+age_scaled_param = scaler.fit(df['Age'].values.reshape(-1, 1))
+df['Age_scaled'] = scaler.fit_transform(df['Age'].values.reshape(-1, 1), age_scaled_param)
+
+age_scaled_param = scaler.fit(df['Fare'].values.reshape(-1, 1))
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'].values.reshape(-1, 1), age_scaled_param)
+print(df)
+
+#取出有用数据
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Pclass_.*|Sex_.*')
+train_np = train_df.as_matrix()
+
+y = train_np[:,0]
+x = train_np[:,1:]
+
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(x, y)
+
+#处理测试数据
+data_test = pd.read_csv("./input/test.csv")
+data_test.info()
+
+data_test.loc[(data_test.Fare.isnull()), 'Fare'] = 0
+
+tmp_df = data_test[['Age', 'Parch', 'Fare', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+x = null_age[:, 1:]
+predicted_age = rfr.predict(x)
+data_test.loc[(data_test.Age.isnull()), 'Age'] = predicted_age
+data_test = set_cabin_type(data_test)
+dummies_cabin = pd.get_dummies(data_test['Cabin'], prefix='Cabin')
+dummies_sex = pd.get_dummies(data_test['Sex'], prefix='Sex')
+dummies_pclass = pd.get_dummies(data_test['Pclass'], prefix='Pclsass')
+dummies_embarked = pd.get_dummies(data_test['Embarked'], prefix='Embarked')
+df_test = pd.concat([data_test, dummies_cabin, dummies_embarked, dummies_pclass, dummies_sex], axis=1)
+df_test.drop(['Name', 'Cabin', 'Sex', 'Pclass', 'Ticket', 'Embarked'], axis=1, inplace=True)
+
+age_scaled_param = scaler.fit(df_test['Age'].values.reshape(-1, 1))
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'].values.reshape(-1, 1), age_scaled_param)
+
+fare_scaled_param = scaler.fit(df_test['Fare'].values.reshape(-1, 1))
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'].values.reshape(-1, 1), fare_scaled_param)
+print(df_test)
+
+test_df = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Pclass_.*|Sex_.*')
+test_np = test_df.as_matrix()
+predictions = clf.predict(test_np)
+result = pd.DataFrame({'PassengerId': data_test['PassengerId'].as_matrix(), 'Survived': predictions.astype(np.int32)})
+
+result.to_csv('./input/logistic_regression_predictions.csv', index=False)
+
+data_result = pd.read_csv('./input/logistic_regression_predictions.csv')
+print(data_result)
